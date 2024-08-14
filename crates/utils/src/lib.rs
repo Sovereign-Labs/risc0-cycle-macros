3354b117f3410254f50fdba43c306df98644ef20
Preston Evans <preston.b.evans@gmail.com>
2024-08-14 10:02:47 -0500

    fix feature gating

--- crates/utils/src/lib.rs
+++ crates/utils/src/lib.rs
@@ -2,23 +2,29 @@
 pub use sov_cycle_macros as macros;
 pub mod risc0;
 pub mod sp1;
-use once_cell::sync::Lazy;
-use std::collections::HashMap;
-use std::sync::Mutex;
 
-/// A global hashmap mapping metric names to their values.
-pub static METRICS_HASHMAP: Lazy<Mutex<HashMap<String, (u64, u64)>>> =
-    Lazy::new(|| Mutex::new(HashMap::new()));
+#[cfg(feature = "native")]
+pub use native::{increment_metric, METRICS_HASHMAP};
+#[cfg(feature = "native")]
+mod native {
+    use once_cell::sync::Lazy;
+    use std::collections::HashMap;
+    use std::sync::Mutex;
 
-/// Increments the requested metric by the given value, creating a
-/// new entry in the global map if necessary.
-pub fn increment_metric(metric: String, value: u64) {
-    let mut hashmap = METRICS_HASHMAP.lock().unwrap();
-    hashmap
-        .entry(metric)
-        .and_modify(|(sum, count)| {
-            *sum += value;
-            *count += 1;
-        })
-        .or_insert((value, 1));
+    /// A global hashmap mapping metric names to their values.
+    pub static METRICS_HASHMAP: Lazy<Mutex<HashMap<String, (u64, u64)>>> =
+        Lazy::new(|| Mutex::new(HashMap::new()));
+
+    /// Increments the requested metric by the given value, creating a
+    /// new entry in the global map if necessary.
+    pub fn increment_metric(metric: String, value: u64) {
+        let mut hashmap = METRICS_HASHMAP.lock().unwrap();
+        hashmap
+            .entry(metric)
+            .and_modify(|(sum, count)| {
+                *sum += value;
+                *count += 1;
+            })
+            .or_insert((value, 1));
+    }
 }
